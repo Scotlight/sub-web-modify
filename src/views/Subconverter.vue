47abf6e4e802ffa1eeef83b0a78dbd4df763a7fe
Scotlight <155151440+Scotlight@users.noreply.github.com>
2026-01-31 09:03:48 +0800

    Update Subconverter.vue

--- src/views/Subconverter.vue
+++ src/views/Subconverter.vue
@@ -338,6 +338,8 @@ export default {
           "suo.yt": "https://suo.yt/short",
         },
         customBackend: {
+          "自用专属后端(1)": "http://eriufgytery67iuer.zeabur.app/",
+          "自用专属后端(2)": "http://new.cloudnet.qzz.io/",
           "CM负载均衡后端【vless reality+hy1+hy2】": "https://subapi.cmliussss.net",
           "CM应急备用后端【vless reality+hy1+hy2】": "https://subapi.fxxk.dedyn.io",
           "肥羊增强型后端【vless reality+hy1+hy2】": "https://url.v1.mk",
@@ -1304,3 +1306,4 @@ export default {
   }
 };
 </script>
+
